45ff57713c29a6c0384956c80f6ce19d7dd625dd
Alp <alpersyuleyman2002@gmail.com>
2023-06-20 12:23:49 +0200

    setting up navbar

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Alper
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
